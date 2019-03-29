018504f254796ff460411f22c5558a233424609c
Lnola <nola.luka@gmail.com>
2019-03-29 22:16:18 +0100

    Ambergher done :)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,21 @@
 window.addEventListener("scroll", () => {
-  if(window.scrollY >= 100){
+  if (window.scrollY >= 100) {
     document.getElementsByTagName("nav")[0].classList.add("background-navy");
   }
-  else{
+  else {
     document.getElementsByTagName("nav")[0].classList.remove("background-navy");
   }
 });
+
+document.getElementsByClassName("header__menu")[0].addEventListener("click", () => {
+  document.getElementById("popup__background").classList.remove("d-none-popup");
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+});
+
+let childrenArray = document.getElementById("popup__background").childNodes;
+
+for (let child of childrenArray)
+  child.addEventListener("click", () => {
+    document.getElementById("popup__background").classList.add("d-none-popup");
+    document.getElementsByTagName("body")[0].style.overflow = "visible";
+  });
